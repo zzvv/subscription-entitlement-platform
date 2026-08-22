fe4cfa1be52d12433e79d012340e46f719d6ddfb
trajectory-base <trajectory-base@local>
2026-08-22 19:38:14 +0800

    导出 004 干净 BUG BASE

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/subscription-entitlement-platform
+
+go 1.22
---
+++ internal/transport/middleware.go
@@ -0,0 +1,182 @@
+package transport
+
+// Component19 encapsulates middleware responsibilities.
+type Component19 struct {
+	ID      string
+	Enabled bool
+	Version int
+}
+
+func NewComponent19(id string) Component19 {
+	return Component19{ID: id, Enabled: true, Version: 1}
+}
+
+func (c Component19) Check01(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check02(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check03(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check04(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check05(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check06(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check07(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check08(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check09(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check10(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check11(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check12(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check13(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check14(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check15(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check16(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
+
+func (c Component19) Check17(value string) string {
+	if !c.Enabled {
+		return "disabled:" + value
+	}
+	if value == "" {
+		return c.ID
+	}
+	return c.ID + "/" + value
+}
